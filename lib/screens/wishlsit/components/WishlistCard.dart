f6c0e80521bd5cb979fbb8ab6e0b24080c02f870
Mazharaliruk <mazharjameel1020@gmail.com>
2023-08-16 18:52:03 +0500

    wishlist changed

--- lib/screens/wishlsit/components/WishlistCard.dart
+++ lib/screens/wishlsit/components/WishlistCard.dart
@@ -3,9 +3,9 @@ import 'package:ecommerce/provider/CartProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '/models/Cart.dart';
-
 import '../../../constants.dart';
 import '../../../size_config.dart';
+
 
 class WishlistCard extends StatelessWidget {
   WishlistCard({
@@ -35,33 +35,33 @@ class WishlistCard extends StatelessWidget {
             child: Container(
               padding: EdgeInsets.all(getProportionateScreenWidth(10)),
               decoration: BoxDecoration(
-                color: Color(0xFFF5F6F9),
+                color: const Color(0xFFF5F6F9),
                 borderRadius: BorderRadius.circular(15),
               ),
               child: CachedNetworkImage(
                 imageUrl: image,
-                placeholder: (context, url) => CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
+                placeholder: (context, url) => const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
             ),
           ),
         ),
-        SizedBox(width: 20),
+        const SizedBox(width: 20),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               productName,
-              style: TextStyle(color: Colors.black, fontSize: 16),
+              style: const TextStyle(color: Colors.black, fontSize: 16),
               maxLines: 2,
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             Consumer<CartProvider>(
                 builder: (context,cartItems,child){
                   return Text.rich(
                     TextSpan(
                       text: "\$${price}",
-                      style: TextStyle(
+                      style: const TextStyle(
                           fontWeight: FontWeight.w600, color: kPrimaryColor),
                     ),
                   );
